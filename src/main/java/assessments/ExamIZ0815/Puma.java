bbbd6e1d9cc0ebe7c450633835764ba6c45ff043
v-Muddu <vishwanath.muddu@gmail.com>
2022-09-27 20:56:49 -0500

    Add solution to question 2 from assessment 1

---
+++ src/main/java/assessments/ExamIZ0815/Puma.java
@@ -0,0 +1,39 @@
+package assessments.ExamIZ0815;
+
+
+public class Puma {
+/**
+ * What is the output of the following program? (Choose all that apply)
+ *
+ * interface HasTail {
+ *     int getTailLength();
+ * }
+ *
+ * abstract class Puma implements HasTail {
+ *     protected int getTailLength() {
+ *         return 4;
+ *     }
+ * }
+ *
+ * public class Cougar implements HasTail {
+ *     public static void main(String[] args) {
+ *         var puma = new Puma(); <- You can't initialize an abstract class
+ *         System.out.println(puma.getTailLength()); <- Compile time error
+ *     }
+ *
+ *     @Override
+ *     public int getTailLength() {
+ *         return 2;
+ *     }
+ * }
+ *
+ * A. 2
+ * B. 4
+ * C. The code will not compile because of line 3. <-
+ * D. The code will not compile because of line 5.
+ * E. The code will not compile because of line 7. <-
+ * F. The code will not compile because of line 10.
+ * G. The output cannot be determined from the code provided. <-
+ *
+ */
+}
